4f297ab610c450a0141f64609c60f0c84cbd060a
Oğuzhan  Çevik <oguzhan728@gmail.com>
2023-08-09 14:17:11 +0300

    remove changes

--- hooks/useStage.js
+++ hooks/useStage.js
@@ -2,7 +2,7 @@
 import React, { useState, useRef, useContext, useEffect } from "react";
 import { LocalMediaContext } from "../contexts/LocalMediaContext.js";
 import Strategy from "../util/strategy.js";
-import {
+import IVSBroadcastClient, {
   Stage,
   StageConnectionState,
   StageEvents,
@@ -10,6 +10,13 @@ import {
 } from "amazon-ivs-web-broadcast";
 
 export default function useStage() {
+  const client = IVSBroadcastClient.create({
+    // Enter the desired stream configuration
+    streamConfig: IVSBroadcastClient.BASIC_LANDSCAPE,
+    // Enter the ingest endpoint from the AWS console or CreateChannel API
+    ingestEndpoint: "435def77beb6.global-contribute.live-video.net",
+  });
+  const streamConfig = IVSBroadcastClient.BASIC_LANDSCAPE;
   const [stageJoined, setStageJoined] = useState(false);
   const [participants, setParticipants] = useState(new Map());
   const [localParticipant, setLocalParticipant] = useState({});
@@ -20,6 +27,15 @@ export default function useStage() {
   const strategyRef = useRef(
     new Strategy(currentAudioDevice, currentVideoDevice)
   );
+
+  const refreshVideoPositions = () =>
+    participants.forEach((participant, index) =>
+      client.updateVideoDeviceComposition(`video-${participant.id}`, {
+        index: 0,
+        width: streamConfig.maxResolution.width / participants.length,
+        x: index * (streamConfig.maxResolution.width / participants.length),
+      })
+    );
 
   useEffect(() => {
     strategyRef.current.updateMedia(currentAudioDevice, currentVideoDevice);
@@ -48,23 +64,47 @@ export default function useStage() {
     }
   };
 
-  const handleMediaAdded = (participantInfo, streams) => {
-    if (!isLocalParticipant(participantInfo)) {
-      const { id } = participantInfo;
-      let participant = participants.get(id);
-      participant = {
-        ...participant,
-        streams: [...streams, ...participant.streams],
-      };
-      setParticipants(new Map(participants.set(id, participant)));
-    }
+  const handleMediaAdded = async (participantInfo, streams) => {
+    const { id } = participantInfo;
+    let participant = participants.get(id);
+    participant = {
+      ...participant,
+      streams: [...streams, ...participant?.streams],
+    };
+    setParticipants(new Map(participants.set(id, participant)));
+    await Promise.all([
+      ...streams
+        .filter((stream) => stream.streamType === StreamType.VIDEO)
+        .map((stream) =>
+          client.addVideoInputDevice(
+            new MediaStream([stream.mediaStreamTrack]),
+            `video-${participant.id}`,
+            {
+              index: 0,
+              width: streamConfig.maxResolution.width / participants.length,
+              x:
+                (participants.length - 1) *
+                (streamConfig.maxResolution.width / participants.length),
+            }
+          )
+        ),
+      ...streams
+        .filter((stream) => stream.streamType === StreamType.AUDIO)
+        .map((stream) =>
+          client.addAudioInputDevice(
+            new MediaStream([stream.mediaStreamTrack]),
+            `audio-${participant.id}`
+          )
+        ),
+    ]);
+    refreshVideoPositions();
   };
 
   const handleMediaRemoved = (participantInfo, streams) => {
     if (!isLocalParticipant(participantInfo)) {
       const { id } = participantInfo;
       let participant = participants.get(id);
-      const newStreams = participant.streams.filter(
+      const newStreams = participant?.streams.filter(
         (existingStream) =>
           !streams.find(
             (removedStream) => existingStream.id === removedStream.id
@@ -72,6 +112,9 @@ export default function useStage() {
       );
       participant = { ...participant, streams: newStreams };
       setParticipants(new Map(participants.set(id, participant)));
+      client.removeVideoInputDevice(`video-${participant.id}`);
+      client.removeAudioInputDevice(`audio-${participant.id}`);
+      refreshVideoPositions();
     }
   };
 
@@ -105,6 +148,9 @@ export default function useStage() {
     }
     try {
       const stage = new Stage(token, strategyRef.current);
+      client.enableVideo();
+      client.enableAudio();
+      
       stage.on(
         StageEvents.STAGE_CONNECTION_STATE_CHANGED,
         handleConnectionStateChange
